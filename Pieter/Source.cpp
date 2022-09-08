aca3efb13d80568f6c975a27875850ac7125ba31
xdjon <xdjon@LAPTOP-PF5PGTIE>
2022-09-08 10:42:20 +0200

    Add project files.

---
+++ Pieter/Source.cpp
@@ -0,0 +1,196 @@
+#include <stdio.h>
+
+
+// 2.17 a
+int main(void)
+/*
+ {
+	int integer1;
+	int integer2;
+	int integer3;
+
+	printf(" Enter u\n");
+	scanf_s("%d", &integer1);
+
+	printf(" Enter a\n");
+	scanf_s("%d", &integer2);
+
+	printf("Enter t\n");
+	scanf_s("%d", &integer3);
+
+	int sum;
+	sum = integer1 + (integer2 * integer3);
+
+	printf("sum is %d\n", sum);
+
+}
+*/
+
+// 2.17 b
+/*
+{
+	int integer1;
+	int integer2;
+	int integer3;
+
+	printf(" Enter u\n");
+	scanf_s("%d", &integer1);
+
+	printf(" Enter a\n");
+	scanf_s("%d", &integer2);
+
+	printf("Enter t\n");
+	scanf_s("%d", &integer3);
+
+	int sum;
+	sum = (integer1*integer2) + 0.5*integer2*(integer3*integer3);
+
+	printf("sum is %d\n", sum);
+
+} 
+
+// 2.19
+/*
+{
+	int integer1;
+	int integer2;
+	int integer3;
+
+	printf(" Enter 3 different integers:\n");
+	scanf_s("%d", &integer1);
+	scanf_s("%d", &integer2);
+	scanf_s("%d", &integer3);
+
+	int sum;
+	sum = integer1 + integer2 + integer3;
+
+	int average;
+	average = ((integer1 + integer2 + integer3) / 3);
+
+	int product;
+	product = integer1 * integer2 * integer3;
+
+
+
+	printf("Sum is %d\n", sum);
+	printf("Average is %d\n", average);
+	printf("Product is %d\n", product);
+
+	if (integer1 < integer2 && integer1 < integer3)
+		printf("Smallest is %d\n", integer1);
+	else if (integer2 < integer1 && integer2 < integer3)
+		printf("Smallest is %d\n", integer2);
+	else if (integer3 < integer1 && integer3 < integer2)
+		printf("Smallest is %d\n", integer3);
+
+	if (integer1 > integer2 && integer1 > integer3)
+		printf("Largest is %d", integer1);
+	else if (integer2 > integer1 && integer2 > integer3)
+		printf("Largest is %d", integer2);
+	else if (integer3 > integer1 && integer3 > integer2)
+		printf("Largest is %d", integer3);
+}
+*/
+
+{
+	int num0;
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int num5;
+	int num6;
+	int num7;
+	int num8;
+	int num9;
+	int num10;
+
+	num0 = 0;
+	num1 = 1;
+	num2 = 2;
+	num3 = 3;
+	num4 = 4;
+	num5 = 5;
+	num6 = 6;
+	num7 = 7;
+	num8 = 8;
+	num9 = 9;
+	num10 = 10;
+
+	printf("Number square cube\t\n");
+
+	int kwadraat0;
+	kwadraat0 = num0 * num0;
+	int cube0;
+	cube0 = num0 * num0 * num0;
+	printf("%d\t%d\t%d\n", num0, kwadraat0, cube0);
+
+	int kwadraat1;
+	kwadraat1 = num1 * num1;
+	int cube1;
+	cube1 = num1 * num1 * num1;
+	printf("%d\t%d\t%d\n", num1, kwadraat1, cube1);
+
+	int kwadraat2;
+	kwadraat2 = num2 * num2;
+
+	int cube2;
+	cube2 = num2 * num2 * num2;
+	printf("%d\t%d\t%d\n", num2, kwadraat2, cube2);
+
+	int kwadraat3;
+	kwadraat3 = num3 * num3;
+
+	int cube3;
+	cube3 = num3 * num3 * num3;
+	printf("%d\t%d\t%d\n", num3, kwadraat3, cube3);
+
+	int kwadraat4;
+	kwadraat4 = num4 * num4;
+
+	int cube4;
+	cube4 = num4 * num4 * num4;
+	printf("%d\t%d\t%d\n", num4, kwadraat4, cube4);
+
+	int kwadraat5;
+	kwadraat5 = num5 * num5;
+
+	int cube5;
+	cube5 = num5 * num5 * num5;
+	printf("%d\t%d\t%d\n", num5, kwadraat5, cube5);
+
+	int kwadraat6;
+	kwadraat6 = num6 * num6;
+
+	int cube6;
+	cube6 = num6 * num6 * num6;
+	printf("%d\t%d\t%d\n", num6, kwadraat6, cube6);
+
+	int kwadraat7;
+	kwadraat7 = num7 * num7;
+
+	int cube7;
+	cube7 = num7 * num7 * num7;
+	printf("%d\t%d\t%d\n", num7, kwadraat7, cube7);
+
+	int kwadraat8;
+	kwadraat8 = num8 * num8;
+
+	int cube8;
+	cube8 = num8 * num8 * num8;
+	printf("%d\t%d\t%d\n", num8, kwadraat8, cube8);
+
+	int kwadraat9;
+	kwadraat9 = num9 * num9;
+
+	int cube9;
+	cube9 = num9 * num9 * num9;
+	printf("%d\t%d\t%d\n", num9, kwadraat9, cube9);
+
+	int kwadraat10;
+	kwadraat10 = num10 * num10;
+
+	int cube10;
+	cube10 = num10 * num10 * num10;
+	printf("%d\t%d\t%d\n", num10, kwadraat10, cube10);
+} 
